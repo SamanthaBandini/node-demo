d7088eebcbc31ed7af8084186680a6703f895ca5
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2018-09-23 15:48:10 +0200

    Minors

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello world!!');
-app.listen (port, () => console.log('Server is running on port '+ port));
+app.get('/', function (req, res) {
+	res.send('Hello world!!')
+	});
+app.listen (port, function(){
+	console.log('Server is running on port '+ port)
+	});
