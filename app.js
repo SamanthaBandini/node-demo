b8e4520c996d50d68fb76a12e8df17fb5af95d6e
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2018-09-23 16:14:40 +0200

    Test variable anc

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-	res.send('Hello world!!')
+	res.send('App Setting: '+ process.env.APPSETTING_MyAppSettings + ' Connection String: ' + process.env.CUSTOMCONNSTR_MyConnectionString));
 	});
 app.listen (port, function(){
 	console.log('Server is running on port '+ port)
