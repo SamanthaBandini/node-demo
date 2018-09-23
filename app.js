48e71a898a42608db6469a87a8297f5b97f04902
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2018-09-23 16:15:51 +0200

    Test variable and connection string

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-	res.send('App Setting: '+ process.env.APPSETTING_MyAppSettings + ' Connection String: ' + process.env.CUSTOMCONNSTR_MyConnectionString));
+	res.send('App Setting: '+ process.env.APPSETTING_MyAppSettings + ' - Connection String: ' + process.env.CUSTOMCONNSTR_MyConnectionString));
 	});
 app.listen (port, function(){
 	console.log('Server is running on port '+ port)
