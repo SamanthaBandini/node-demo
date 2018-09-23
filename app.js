b3b9860accaba140d77bfb8655f297ae6eec962e
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2018-09-23 16:25:53 +0200

    Print env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-	res.send('App Setting: ');
+	res.send(process.env);
 	});
 app.listen (port, function(){
 	console.log('Server is running on port '+ port)
