259c94867752a9eeeed7a813085b87f9e147f7e6
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2018-09-23 16:36:49 +0200

    Revert "Insert variables"
    This reverts commit d995e9ddf1426beec0830ffc085e8a86ae602e83.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-	res.send('App Setting: '+ process.env.APPSETTING_MyAppSettings + ' - Connection String: ' + process.env.CUSTOMCONNSTR_MyConnectionString));
+	res.send(process.env);
 	});
 app.listen (port, function(){
 	console.log('Server is running on port '+ port)
