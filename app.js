6a2d1e966b5ab0812f3b9608736a2b1e30544411
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2018-09-23 17:01:05 +0200

    Add post for meraki

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var port = 9201;													//TCP listening port
+//TODO: Change port for meraki
+var portForMeraki = 9201;													//TCP listening port
 var secret = "testmeraki";											//Secret that you chose in the Meraki dashboard
 var validator = "2521959f5342db521bab4454c1e839d121de38c0";			//Validator string that is shown in the Meraki dashboard
 
@@ -19,6 +20,26 @@ app.get('/', function (req, res) {
 app.get('/meraki', function(req, res){
   res.send(validator);
   console.log("sending validation")
+});
+
+//TODO: To be checked
+app.post('/meraki', function(req, res){ 
+	try {
+	  var jsoned = JSON.parse(req.body.data);
+	  if (jsoned.secret == secret) {
+		  for (i=0; i<jsoned.probing.length; i++) {
+			  console.log("client " + jsoned.probing[i].client_mac + " seen on ap " + jsoned.probing[i].ap_mac + " with rssi " + jsoned.probing[i].rssi + " at " + jsoned.probing[i].last_seen);
+		  }
+	   } else {
+		   console.log("invalid secret from  " + req.connection.remoteAddress);
+	   }
+	} catch (e) {
+		// An error has occured, handle it, by e.g. logging it
+  	console.log("Error.  Likely caused by an invalid POST from " + req.connection.remoteAddress + ":");
+  	console.log(e);
+  	res.end();
+  }
+  
 });
 
 app.listen (port, function(){
