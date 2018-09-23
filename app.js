9db4f870952a5ac172b6ecde6c4f0a7881b41cba
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2018-09-23 16:21:04 +0200

    return to var

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
 	res.send('App Setting: '+ process.env);
 	});
