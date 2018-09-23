883ce809a583aa9bd68166b320db5b8b60eaef36
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2018-09-23 16:24:17 +0200

    Print only string

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-	res.send('App Setting: '+ process.env);
+	res.send('App Setting: ');
 	});
 app.listen (port, function(){
 	console.log('Server is running on port '+ port)
