fc219dce53b2abb9e27edc5b411c17ac579754cf
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2018-09-23 16:42:23 +0200

    Modify for meraki

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+
 const express = require('express');
 const app = express();
+
+var port = 9201;													//TCP listening port
+var secret = "testmeraki";											//Secret that you chose in the Meraki dashboard
+var validator = "2521959f5342db521bab4454c1e839d121de38c0";			//Validator string that is shown in the Meraki dashboard
+
 var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
 	res.send(process.env);
 	});
+	
+app.get('/meraki', function(req, res){
+  res.send(validator);
+  console.log("sending validation")
+});
+
 app.listen (port, function(){
 	console.log('Server is running on port '+ port)
 	});
